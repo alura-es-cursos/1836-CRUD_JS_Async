6570d486fab3356a840b62e89d592d0187837fd1
HarlandLohora <harland@lohora.com>
2021-07-14 02:29:26 -0300

    clase03 completada

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,9 +1,7 @@
 import { clientServices } from "../service/client-service.js";
 
-console.log(clientServices);
-
 //backticks
-const crearNuevaLinea = (nombre, email) => {
+const crearNuevaLinea = (nombre, email, id) => {
   const linea = document.createElement("tr");
   const contenido = `
     <td class="td" data-td>
@@ -21,7 +19,7 @@ const crearNuevaLinea = (nombre, email) => {
           </a>
         </li>
         <li>
-          <button class="simple-button simple-button--delete" type="button">
+          <button class="simple-button simple-button--delete" type="button" id="${id}">
             Eliminar
           </button>
         </li>
@@ -29,6 +27,17 @@ const crearNuevaLinea = (nombre, email) => {
     </td>
   `;
   linea.innerHTML = contenido;
+  const btn = linea.querySelector("button");
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+    clientServices
+      .eliminarCliente(id)
+      .then((respuesta) => {
+        console.log(respuesta);
+      })
+      .catch((err) => alert("Ocurrió un error"));
+  });
+
   return linea;
 };
 
@@ -37,8 +46,8 @@ const table = document.querySelector("[data-table]");
 clientServices
   .listaClientes()
   .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+    data.forEach(({ nombre, email, id }) => {
+      const nuevaLinea = crearNuevaLinea(nombre, email, id);
       table.appendChild(nuevaLinea);
     });
   })
--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,24 @@
 const listaClientes = () =>
   fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
+  });
+};
+
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
+
 export const clientServices = {
   listaClientes,
+  crearCliente,
+  eliminarCliente,
 };
