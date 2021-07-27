1c46d252bc2aae6b12525dc5064561d8e19ea037
HarlandLohora <harland@lohora.com>
2021-07-26 23:24:24 -0300

    clase05 completada

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
   const url = new URL(window.location);
   const id = url.searchParams.get("id");
 
@@ -13,10 +13,17 @@ const obtenerInformacion = () => {
   const nombre = document.querySelector("[data-nombre]");
   const email = document.querySelector("[data-email]");
 
-  clientServices.detalleCliente(id).then((perfil) => {
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-  });
+  try {
+    const perfil = await clientServices.detalleCliente(id);
+    if (perfil.nombre && perfil.email) {
+      nombre.value = perfil.nombre;
+      email.value = perfil.email;
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    window.location.href = "/screens/error.html";
+  }
 };
 
 obtenerInformacion();
